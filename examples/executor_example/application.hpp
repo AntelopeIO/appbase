329d0416f8d10a2df4d60f66187fd9dbe41baa5e
Lin Huang <lin.huang@eosnetwork.com>
2023-02-20 17:47:48 -0500

    add two-queue executor support; add corresponding tests

--- examples/executor_example/application.hpp
+++ examples/executor_example/application.hpp
@@ -21,7 +21,7 @@ class my_executor {
 public:
    template <typename Func>
    auto post( int priority, Func&& func ) {
-      return boost::asio::post(io_serv, pri_queue.wrap(priority, std::forward<Func>(func)));
+      return boost::asio::post(io_serv, pri_queue.wrap(priority, order, std::forward<Func>(func)));
    }
 
    auto& get_priority_queue() { return pri_queue; }
@@ -36,6 +36,7 @@ private:
    // members are ordered taking into account that the last one is destructed first
    boost::asio::io_service  io_serv;
    appbase::execution_priority_queue pri_queue;
+   std::size_t order = std::numeric_limits<size_t>::max(); // to maintain FIFO ordering in queue within priority
 };
 
 namespace appbase {
